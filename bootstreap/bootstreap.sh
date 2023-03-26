8e82521673c90eb3198f4c4808baae91978bbdef
weihongyu1 <488009667@qq.com>
2023-03-26 19:48:37 +0800

    修改启动脚本呢

--- bootstreap/bootstreap.sh
+++ bootstreap/bootstreap.sh
@@ -2,12 +2,14 @@
 
 LOCAL_TIME=$(date+"%Y%m%d %H:%M:%S")
 HEALTH='健康检查'
-echo "${HEALTH} ${LOCAL_TIME}" >> health.log
-RESULT=$(curl -s http://localhost/ts/health)
-if [ ${RESULT} -eq 200 ];
+echo ${HEALTH}
+pIDa=`/usr/sbin/lsof -i :80 | grep -v "PID" | awk '{print $2}'`
+echo $pIDa
+if [ "$pIDa" != "" ];
 then
-  echo '健康检查通过' >> health.log
+   echo '健康检查通过'
 else
-  echo '健康检查不通过' >> health.log
-  nohup java -jar /home/java/transport/transportsecurity/target/transportsecurity_finally-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+   echo '健康检查不通过'
+   nohup java -jar /home/java/transport/transportsecurity/target/transportsecurity_finally-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod &
+   echo '程序启动完成'
 fi
